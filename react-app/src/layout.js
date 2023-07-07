1e92ef1fa35671fd0323383ae41a37ade74c76c1
KevinH48264 <1kevin.huang@gmail.com>
2023-07-07 15:40:29 -0400

    somewhat working force directed graph

--- react-app/src/layout.js
+++ react-app/src/layout.js
@@ -1,6 +1,47 @@
 import { forceSimulation, forceManyBody, forceCenter, forceCollide, forceLink } from 'd3-force';
 
-export function computeLayout(paperNodes, edgesData, leafClusters) {
+function forceBoundedBox() {
+    var nodes, sizes
+    var bounds = [[0, 0], [1, 1]] // default bounds
+
+    function force() {
+        var node, size;
+        var xi, x0 = bounds[0][0], x1 = bounds[1][0];
+        var yi, y0 = bounds[0][1], y1 = bounds[1][1];
+
+        for (var i = 0, n = nodes.length; i < n; ++i) {
+            node = nodes[i];
+            size = sizes[i];
+            xi = node.x || 0;
+            yi = node.y || 0;
+            if (xi < x0 || xi > x1) { node.x = Math.max(x0, Math.min(x1, xi)); }
+            if (yi < y0 || yi > y1) { node.y = Math.max(y0, Math.min(y1, yi)); }
+        }
+    }
+
+    function initialize() {
+        if (!nodes) return;
+        sizes = new Array(nodes.length); 
+        // if you have varying sizes per node, compute them here using nodes[i].radius or similar
+        for (var i = 0, n = nodes.length; i < n; ++i) {
+            sizes[i] = 1; // replace 1 with node size if you have varying sizes
+        }
+    }
+
+    force.initialize = function(_) {
+        nodes = _;
+        initialize();
+    };
+
+    force.bounds = function(_) {
+        return arguments.length ? (bounds = _, force) : bounds;
+    };
+
+    return force;
+}
+
+
+export function computeLayout(paperNodes, edgesData, leafClusters, minX, minY, maxX, maxY) {
     // Create dummy 'center' nodes and add them to paperNodes first
     leafClusters.forEach(cluster => {
         // Create dummy 'center' node
@@ -20,6 +61,7 @@ export function computeLayout(paperNodes, edgesData, leafClusters) {
         // TODO: this is usually determined by the radius, update radius if not citationCount!
       return Math.sqrt(d.citationCount);
     }))
+    .force('bounded', forceBoundedBox().bounds([[minX, minY], [maxX, maxY]])) // add the bounded box force
     .stop();
 
   let links = [];
