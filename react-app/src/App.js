72419cc1a7c7ea1766d2fd4e17b226e8cf167ecd
KevinH48264 <1kevin.huang@gmail.com>
2023-07-15 12:06:59 -0400

    added code for MST and top 2 paper connections

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
           console.log("Papers data:", json);
       })
 
-    fetch('gpt_classified_100/single_connected_edges_100_semantic_scholar.json')
+    fetch('gpt_classified_100/single_connected_top_2_edges_100_semantic_scholar.json')
       .then(response => response.json())
       .then(json => {
         setEdgesData(json);
