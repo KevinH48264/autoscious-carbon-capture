5b6ab0cb733c53ce9454d13bf7e165159624b169
KevinH48264 <1kevin.huang@gmail.com>
2023-07-17 15:18:29 -0400

    added more nice looking colors

--- react-app/src/util.js
+++ react-app/src/util.js
@@ -1,4 +1,5 @@
 import { Rectangle } from 'pixi.js';
+import chroma from 'chroma-js';
 
 export const randomDarkModeColor = () => {
     // vary hue between 0 and 360 degrees
@@ -249,311 +250,6 @@ export function getVoronoiNeighbors(voronoi, i) {
   return Array.from(voronoi.neighbors(i));
 }  
 
-// Random colors (many)
-export const circleColorDiamondSequence = [
-    14413679,
-    8130704,
-    11765267,
-    2811802,
-    1221959,
-    15148973,
-    2025023,
-    1290288,
-    5957152,
-    15195948,
-    13494055,
-    13047398,
-    2453478,
-    13298244,
-    10874478,
-    1477575,
-    8952337,
-    4711914,
-    11360531,
-    2233264,
-    13355030,
-    10606616,
-    6199825,
-    3120657,
-    5115786,
-    4254027,
-    15150256,
-    15489713,
-    3401827,
-    3532992,
-    10455825,
-    14424184,
-    15383624,
-    15300926,
-    1066133,
-    15556767,
-    6679111,
-    10517009,
-    14108439,
-    13756737,
-    7112174,
-    6942351,
-    15232560,
-    13506111,
-    1405378,
-    9113438,
-    1472461,
-    9375616,
-    13703959,
-    15148865,
-    2278118,
-    10687100,
-    1290629,
-    1288997,
-    11867853,
-    13691458,
-    2297743,
-    6730478,
-    14151536,
-    12447073,
-    10546267,
-    1329076,
-    9916432,
-    6469396,
-    3598565,
-    4174057,
-    1396417,
-    15498584,
-    1659873,
-    11735978,
-    9170799,
-    15285888,
-    1535703,
-    12840025,
-    13198102,
-    6654735,
-    7007908,
-    1019196,
-    15709807,
-    12175638,
-    5941740,
-    1324982,
-    15197485,
-    14008599,
-    6427305,
-    3527447,
-    10162574,
-    12207338,
-    5059560,
-    13049830,
-    5499730,
-    14525720,
-    11870950,
-    15150511,
-    6084076,
-    2221714,
-    1153554,
-    9049275,
-    15444303,
-    11817707,
-    12840285,
-    7181294,
-    1087345,
-    11408147,
-    1018402,
-    13671959,
-    15625099,
-    11094250,
-    2429073,
-    2352849,
-    2238694,
-    15421518,
-    13572313,
-    15372874,
-    12391711,
-    1351608,
-    14424173,
-    7048207,
-    10490220,
-    15168295,
-    15043867,
-    13888336,
-    11425811,
-    2221681,
-    5968287,
-    15693002,
-    10131217,
-    15422050,
-    2483868,
-    5332715,
-    15692402,
-    11868379,
-    2910951,
-    3269915,
-    3055889,
-    15365961,
-    8874478,
-    9284371,
-    6061548,
-    4043497,
-    7410607,
-    9900442,
-    1085486,
-    15081094,
-    5106575,
-    4307177,
-    1087051,
-    3467463,
-    8688912,
-    1356956,
-    4582100,
-    12538347,
-    8162575,
-    15427919,
-    1403071,
-    15238454,
-    15588453,
-    5485844,
-    15150169,
-    6089921,
-    11080284,
-    14565912,
-    15150471,
-    12505111,
-    5630188,
-    4426255,
-    15694448,
-    4170217,
-    4123046,
-    2680690,
-    5696727,
-    1345204,
-    11261976,
-    9345553,
-    3171816,
-    13917207,
-    6152429,
-    9411601,
-    6733294,
-    7375631,
-    13261334,
-    15218901,
-    5827778,
-    2723856,
-    8187236,
-    8318566,
-    1415877,
-    15589474,
-    15111206,
-    1493950,
-    12380212,
-    8720269,
-    4758545,
-    1493029,
-    4527556,
-    8434195,
-    15217096,
-    15065114,
-    10162585,
-    10656530,
-    6679378,
-    3806657,
-    13195500,
-    10687091,
-    14725656,
-    4647583,
-    15691904,
-    15150409,
-    15556519,
-    6691033,
-    15149890,
-    7334470,
-    2465809,
-    1331125,
-    4332261,
-    11661170,
-    15692665,
-    12327133,
-    11539378,
-    15285636,
-    15378502,
-    1289108,
-    2615252,
-    15286600,
-    14412356,
-    1426821,
-    1129120,
-    8852667,
-    4188505,
-    8394974,
-    3205181,
-    1425503,
-    1425497,
-    15712369,
-    4199077,
-    15186219,
-    10217259,
-    11152615,
-    3412910,
-    1630140,
-    1515219,
-    7466910,
-    15285165,
-    7902480,
-    14817747,
-    12460821,
-    15357258,
-    5093396,
-    1330105,
-    11604891,
-    15353533,
-    11195927,
-    7597807,
-    1290339,
-    13375126,
-    10424994,
-    15285634,
-    14358690,
-    10950077,
-    8460243,
-    15431250,
-    15625594,
-    15420512,
-    7378159,
-    1493783,
-    1489612,
-    6217453,
-    2103250,
-    1186218,
-    2693336,
-    7479014,
-    2004752,
-    3008414,
-    5892148,
-    7532469,
-    12391859,
-    15436364,
-    15217769,
-    1124257,
-    6427832,
-    10163122,
-    5510329,
-    10252014,
-    13560124,
-    9572469,
-    7611366,
-    14740035,
-    2876966,
-    5574538,
-    10774290,
-    11801783,
-    12063919,
-    1511569,
-    1355961,
-    12040980,
-    15420649,
-    6674925,
-    4353514,
-    8132030,
-    1358226,
-    1696934
-]
-
 // Diamond colors
 export const diamondPolygonColorSequence = [
   0xB9F2FF, // Very Pale Blue
@@ -567,6 +263,93 @@ export const diamondPolygonColorSequence = [
   0xB5E0D3, // Light Cyan
   0x92CEC5  // Medium Aquamarine
 ];
+
+// Define your base colors for each top level category.
+// You may need to adjust these depending on the number of top levels you have.
+// const baseColors = [
+//   "#00008B",  // Dark Blue
+//   "#8B0000",  // Dark Red
+//   "#008B8B",  // Dark Cyan
+//   "#6B8E23",  // Olive Drab
+//   "#8B008B",  // Dark Magenta
+//   "#2F4F4F",  // Dark Slate Grey
+//   "#A0522D",  // Sienna
+//   "#708090",  // Slate Grey
+//   "#4682B4",  // Steel Blue
+//   "#B22222",  // Firebrick
+//   "#228B22",  // Forest Green
+//   "#8B4513",  // Saddle Brown
+//   "#7B68EE",  // Medium Slate Blue
+//   "#800080",  // Purple
+//   "#000000"   // Black
+// ];
+
+// const colorMap = {};
+
+// export const getColorForClass = (classId) => {
+//     const parts = classId.split(".");
+//     const topLevelIndex = parseInt(parts[0], 10) - 1;
+//     const colorIndex = topLevelIndex % baseColors.length;  // use modulus to wrap around
+    
+//     // If we haven't seen this top level before, assign it a base color and create a scale
+//     if (!colorMap[parts[0]]) {
+//         colorMap[parts[0]] = {
+//             base: baseColors[colorIndex],
+//             scale: chroma.scale([chroma(baseColors[colorIndex]), 'white']).mode('lch').colors(10)
+//         };
+//     }
+
+//     // Now get a specific hue for the subtopic based on the subcategory index
+//     let color = colorMap[parts[0]].scale[0];
+//     for (let i = 1; i < parts.length; i++) {
+//         const subcategoryIndex = parseInt(parts[i], 10) - 1;
+//         color = chroma.mix(color, colorMap[parts[0]].scale[subcategoryIndex], 0.5, 'lch');
+//     }
+    
+//     return color;
+// };
+
+const baseColors = [
+  "rgb(0, 0, 139)",    // DarkBlue
+  "rgb(139, 0, 0)",    // DarkRed
+  "rgb(0, 139, 139)",  // DarkCyan
+  "rgb(107, 142, 35)", // OliveDrab
+  "rgb(139, 0, 139)",  // DarkMagenta
+  "rgb(47, 79, 79)",   // DarkSlateGray
+  "rgb(160, 82, 45)",  // Sienna
+  "rgb(112, 128, 144)", // SlateGray
+  "rgb(0, 0, 0)",      // Black
+];
+
+const colorMap = {};
+
+export const getColorForClass = (classId) => {
+  console.log("Getting color for classId", classId)
+  const parts = classId.split(".");
+  const topLevelIndex = parseInt(parts[0], 10) - 1;
+  const colorIndex = topLevelIndex % baseColors.length;  // use modulus to wrap around
+
+  // If we haven't seen this top level before, assign it a base color and create a scale
+  if (!colorMap[parts[0]]) {
+      colorMap[parts[0]] = {
+          base: chroma(baseColors[colorIndex]).hex(),
+          scale: chroma.scale([chroma(baseColors[colorIndex]).hex(), 'white']).mode('lch').colors(10)
+      };
+  }
+
+  // Now get a specific hue for the subtopic based on the subcategory index
+  let color = colorMap[parts[0]].scale[0];
+  for (let i = 1; i < parts.length; i++) {
+      const subcategoryIndex = parseInt(parts[i], 10) - 1;
+      color = chroma.mix(color, colorMap[parts[0]].scale[subcategoryIndex], 0.5, 'lch').hex();
+  }
+  
+  console.log("COLOR: ", color)
+  return color;
+};
+
+
+
 
 // export const assignHues = (nodes, min_hue, max_hue) => {
 //     const range = max_hue - min_hue;
