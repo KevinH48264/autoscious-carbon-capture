b7106970e7c71b25e554545c12352947169e8fcd
KevinH48264 <1kevin.huang@gmail.com>
2023-07-17 16:34:51 -0400

    cleaning up code

--- react-app/src/PapersMap.js
+++ react-app/src/PapersMap.js
@@ -17,8 +17,6 @@ const ResearchPaperPlot = ({ papersData, edgesData, clusterData }) => {
 
     // Compute force-directed layout of PaperNodes
     let paperNodes = papersData
-      // .filter(({paperId, title, abstract}) => paperId != null && title != null && abstract != null)
-      // .slice(0, 30)
       .map(({title, x, y, citationCount, paperId, abstract, classification_ids}) => ({title, x: x, y: y, citationCount, paperId, abstract, classification_ids, main_class: classification_ids[0]}))
     let leafClusters = flattenClusters(clusterData);
     let centroidNodes = []
@@ -32,14 +30,10 @@ const ResearchPaperPlot = ({ papersData, edgesData, clusterData }) => {
     calculateClusterCentroids(leafClusters, layoutNodes, centroidNodes)
     console.log("leaf clusters", leafClusters) // expect 58 leaf clusters bc there are 58 categories
     console.log("centroidNodes", centroidNodes) // expect 58 centroid ndoes for each cluster
-
-    // const layout = computeLayout(paperNodes, edgesData, leafClusters, centroidNodes, edgesData);
-    // paperNodes = layout.paperNodes;
     // const edges = layout.edgesData;
     // const centerNodes = layout.centerNodes; // clusterNode!
     // const normalizedRadius = layout.normalizedRadius; // this should be used to determine the zoom, currently 230, prev 150
     // const zoomScale = normalizedRadius / 300;
-
 
     const app = new PIXI.Application({
       width: window.innerWidth,
@@ -91,11 +85,12 @@ const ResearchPaperPlot = ({ papersData, edgesData, clusterData }) => {
     const minY = Math.min(...layoutNodes.map((paper) => paper.y));
     const maxY = Math.max(...layoutNodes.map((paper) => paper.y));
     const voronoi = delaunay.voronoi([minX - extendFactor, minY - extendFactor, maxX + extendFactor, maxY + extendFactor]);
+
     // scale the data to fit within the worldWidth and worldHeight
     const scaleX = (d) => ((d - minX) / (maxX - minX)) * viewport.worldWidth;
     const scaleY = (d) => ((d - minY) / (maxY - minY)) * viewport.worldHeight;
 
-    // Adding a circle mask
+    // Adding a circle mask and an outline around the circle mask
     let farthestDistance = 0;
     layoutNodes.forEach(node => {
       let distance = Math.sqrt(Math.pow(scaleX(node.x) - viewport.worldWidth / 2, 2) + Math.pow(scaleY(node.y) - viewport.worldHeight / 2, 2));
@@ -107,18 +102,14 @@ const ResearchPaperPlot = ({ papersData, edgesData, clusterData }) => {
     circleMask.beginFill(0x000000); // You can fill with any color
     circleMask.drawCircle(viewport.worldWidth / 2, viewport.worldHeight / 2, farthestDistance + 10);
     circleMask.endFill();
-
-    // Create an outline around the circle mask
     let outline = new PIXI.Graphics();
     outline.beginFill(0x998D76); // Choose the color for your border
     outline.drawCircle(viewport.worldWidth / 2, viewport.worldHeight / 2, farthestDistance + 15); // +15 instead of +10 to make it larger than the mask
-
     outline.beginHole();
     outline.drawCircle(viewport.worldWidth / 2, viewport.worldHeight / 2, farthestDistance + 10); // The size of your circle mask
     outline.endHole();
     outline.endFill();
     viewport.addChild(outline)
-
     const polygonContainer = new PIXI.Container();
     viewport.addChild(polygonContainer);
     polygonContainer.mask = circleMask;
@@ -194,48 +185,6 @@ const ResearchPaperPlot = ({ papersData, edgesData, clusterData }) => {
     // // Sort paperNodes by citationCount to prioritize showing higher citationCount papers
     layoutNodes.sort((a, b) => b.data.citationCount - a.data.citationCount);
 
-    // console.log("clusterCentroids", clusterCentroids)
-
-    // assuming `nodes` is your array of node objects and `edgesData` is your array of edges
-    // let paperIdToNode = {};
-    // paperNodes.forEach(node => {
-    //     paperIdToNode[node.paperId] = node;
-    // });
-    // console.log("paperIdToNode", paperIdToNode)
-
-    // const taxonomy = {
-    //   "Carbon Capture and Storage (CCS) Technologies": {
-    //     "Membrane-Based Technologies": ["df275f0b3dca8552250d569db1bf18de8463c9e9", "deff0e3386caac160ecf968815a2938a542e34f0", "2366c402cf265deb94ed2e6e80375b3d9bf7004c", "cfa569019717301c352aabec292a8f4d3a261553"],
-    //     "Mineralization": ["76a20bec8e313ced0faf8010e6ecbe3965a9e305"],
-    //     "Metal-Organic Frameworks (MOFs)": ["5b190c555003e154bb9038844899014425236273", "9308b1d5019588e96d23b401d2f9df01df00d393", "7fd50091d43cf35198cb0533a7dd3595cec7f1a7", "1aeced2070a2e23757d95aac9ebddf2e0a9d0e09", "e02f812df3cd677ec931b0ce47e10640f16968fa", "9496966a27c7a9176ec0a8ff6a04781caa42b7bb"],
-    //     "Covalent Organic Frameworks": ["7a2197144b3a1567e814ea35fd73bd85a698681f"],
-    //     "Direct Air Capture": ["522986777b3989c34f0ea37a35e1c4f4fb3d65c8"],
-    //     "Pyrogenic": ["24bd758dbdb9ce0ea8e913b56a8224c0389c83fe"]
-    //   },
-    //   "Industry Applications": {
-    //     "Chemical Industry": ["e24d6d882c78389a9c356b87ac46e065087549cf"],
-    //     "Flue Gas Capture": ["19b604c4e00c43cfd916603d20b2532ccb2896ea"],
-    //     "Natural Gas & Hydrogen": ["620999ecca4608a89bd69cbe97ab752ce1e62161"],
-    //     "Electricity Grids": ["f15a2d21309f429fbec688b8aee5c7bdb66b8f40"]
-    //   },
-    //   "Socio-Economic Analysis": {
-    //     "Life Cycle Assessment": ["154b9fd69570539e90f51e1b19db944713d3bfdc", "0ffa1b15908277b77ad7382c7b28f2e63226aabc", "12a27df31fd069ab0db9abcbd7776de7a6fee0eb"],
-    //     "Economic Analyses": ["62519425cbe4ca9578c88b21c57553b3fd5783bb"],
-    //     "Policy Analysis": ["6901beee29fd0c97005ff92a7aefc5c70d19aab5"],
-    //     "Infrastructure": ["be026181508eca3fc0ce54bb3571f4fdbc014a8b"]
-    //   },
-    //   "Carbon Capture from Biological Sources": {
-    //     "Natural Forests": ["1f61df1af871412b52c5fc38902854fbede1a703"],
-    //     "Microalgae Biorefinery": ["57eec80dc233eb48b42b6ef4299acbc565a32f72"],
-    //     "Biorefinery": ["fa71f477a4f42d0dd2b5e3b1d36d35108654ecb7"]
-    //   },
-    //   "Literature Reviews and Updates": {
-    //     "General Analysis": ["e3e850fb87909bd91aab7ecb8260417ccfcb383b"],
-    //     "Literature Review Update": ["e1e15aa932ac61efa8bb0c5cc99cfe6521458861", "3c87b0e4d3d93128fb6d425da491a7fea528b587"]
-    //   }
-    // };
-
-    // Create and add all circles and text to the viewport
     const drawNodes = (nodes, vis_cluster_centroids, viewport) => {
       let zoomLevel = Math.max(-1, Math.round(((viewport.scaled) - 2) * 10))
       let originalZoomLevel = zoomLevel;
