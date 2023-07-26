474605ba27000fc042aec65e02c06618531a973e
KevinH48264 <1kevin.huang@gmail.com>
2023-07-26 19:17:13 -0400

    working update taxonomy and clasify new papers script

--- knowledge_base/README.md
+++ knowledge_base/README.md
@@ -11,31 +11,40 @@
 5. old -- archive
 
 ## 1. For updating knowledge base with new papers: 
-- Edit and run update_database.ipynb.
+- Edit and run update_database.ipynb or run update_database.py
 
 ## 2. For adding embeddings
-- Edit and run generate_viz.ipynb Pre-processing step 1 or edit and run get_embeddings.py
+- Edit and run get_embeddings.py
+- Alternatively, edit and run generate_viz.ipynb Pre-processing step 1
 
 ## 3. For generating latest t-SNE x and y coordinates:
-- Edit and run generate_viz.ipynb Pre-processing step 2 or edit and run get_tsne.py
+- Edit and run get_tsne.py
+- Alternatively, edit and run generate_viz.ipynb Pre-processing step 2
 
 ## 4. For generating a seeded initial taxonomy
 - Edit the prompt and run "python seed_initial_taxonomy.py"
+- Optionally, edit and run generate_viz.ipynb
 
 ## 5. For updating taxonomy and extracting keywords and classifying papers without extracted keywords
-- Edit and run generate_viz.ipynb Pre-processing step 3 or edit and run update_taxonomy_classify_new.py
+- Edit and run update_taxonomy_new_classify.py
+- Alternatively, edit and run generate_viz.ipynb
 
 ## 6. For updating taxonomy and reclassifying extracted keywords
-- Edit and run generate_viz.ipynb Pre-processing step 3 and use process_keywords instead of process_papers or edit and run update_taxonomy_reclassify_keywords.py (under development, currently just reclassifies from the start instead of from lowest confidence score)
+- Edit and run update_taxonomy_reclassify_keywords.py (under development, currently just reclassifies from the start instead of from lowest confidence score)
+- Alternatively, edit and run generate_viz.ipynb
 
 ## 7. For reorganizing taxonomy
 - Edit and run reorganize_taxonomy.py
+- Alternatively, edit and run generate_viz.ipynb
 
 ## 8. For adding confidence scores to classification ids
 - Edit and run add_keyword_class_scores.py
+- Alternatively, edit and run generate_viz.ipynb
 
 ## 9. For generating taxonomy JSON
 - Edit and run gen_taxonomy_json.py
+- Alternatively, edit and run generate_viz.ipynb
 
 ## 10. For generating edges
 - Edit and run gen_edges.py
+- Alternatively, edit and run generate_viz.ipynb
--- knowledge_base/get_embeddings.py
+++ knowledge_base/get_embeddings.py
@@ -1,3 +1,5 @@
+'''This creates text column for entries without text and embeddings for rows without embeddings yet. CTRL+C whenever you want to stop generating embeddings. Saves happen every 500 papers.'''
+
 # From [Pre-processing - 1]
 
 import pandas as pd
@@ -5,45 +7,15 @@ import json
 import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModel
+from datetime import datetime
+import os
 
-# NOTE: Only need to edit this with latest papers JSON from knowledge_base/papers folder and model for pre-processing steps 1 and 2
-# Load in latest JSON files
-papers_json_path = r'C:\Users\1kevi\Desktop\projects\Research\autoscious-carbon-capture\knowledge_base\papers\23-07-25_11935_database_update.json'
+MODEL = "allenai/specter"
+MODEL_NAME_SAVE_SAFE = MODEL.replace("/", "_")  # replace the slash with underscore
 
-tokenizer = AutoTokenizer.from_pretrained("allenai/specter")
-model = AutoModel.from_pretrained("allenai/specter")
-
-# load the data from your JSON file
-with open(papers_json_path, 'r') as f:
-    data = json.load(f)
-
-# convert the data into a pandas DataFrame
-df = pd.DataFrame(data)
-
-print("Before calculating text, # text not None: ", len(df[df['text'].notna()]), "# None: ", len(df[df['text'].isna()]))
-
-# Define the conditions for the 'text' column
-condition = df['text'].isna()
-
-# Create a subset dataframe where 'text' is NaN
-subset_df = df[condition]
-
-# Define what the 'text' column should be if the condition is met
-value_when_true = 'Title: ' + subset_df['title'].astype(str) + '. Abstract: ' + subset_df['abstract'].astype(str)
-
-# Define what the 'text' column should be if the abstract is na
-value_when_abstract_na = 'Title: ' + subset_df['title'].astype(str) + '.'
-
-# Apply the conditions to the DataFrame
-df.loc[condition, 'text'] = np.where(subset_df['abstract'].isna(), value_when_abstract_na, value_when_true)
-
-print("After calculating text, # text not None: ", len(df[df['text'].notna()]), "# None: ", len(df[df['text'].isna()]))
-
-
-print("Before calculating embedding, # embedding not None: ", len(df[df['embedding'].notna()]), "# None: ", len(df[df['embedding'].isna()]))
 
 # Function to get SPECTER embedding
-def get_specter_embedding(text):
+def get_specter_embedding(text, tokenizer, model):
     # Tokenize text
     inputs = tokenizer(text, padding=True, truncation=True, max_length=512, return_tensors='pt')
 
@@ -56,25 +28,65 @@ def get_specter_embedding(text):
 
     return str(embedding_np.tolist())
 
-# Get indices where 'embedding' is None
-embedding_isna_indices = df[df['embedding'].isna()].index
-max_index = embedding_isna_indices.max()
+def get_embeddings():
+    # Load in existing knowledge base from papers or custom papers JSON
+    papers_json_path = 'papers/latest_papers.json'
 
-# Compute SPECTER embeddings for these rows and store in 'embedding' column
-now = datetime.now()
-date_str = now.strftime('%y-%m-%d')
-time_str = now.strftime('%H-%M-%S')
-folder_path = f'papers/{date_str}'
-n = len(papers.keys())
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModel.from_pretrained(MODEL)
 
-for i, idx in enumerate(embedding_isna_indices):
-    print(f"idx {idx} / {max_index}")
-    df.loc[idx, 'embedding'] = get_specter_embedding(df.loc[idx, 'text'])
+    # load the data from your JSON file
+    with open(papers_json_path, 'r') as f:
+        data = json.load(f)
 
-    # save to json in papers and date, labeled by time every 1000 iterations
-    if (i + 1) % 1000 == 0:
-        df.to_json(f'{folder_path}/{time_str}_embeddings_{i}.json')
+    # convert the data into a pandas DataFrame
+    df = pd.DataFrame(data)
 
-print("# embedding not None: ", len(df[df['embedding'].notna()]), "# None: ", len(df[df['embedding'].isna()]))
+    print("Before calculating text, # text not None: ", len(df[df['text'].notna()]), "# None: ", len(df[df['text'].isna()]))
+
+    # Define the conditions for the 'text' column
+    condition = df['text'].isna()
+
+    # Create a subset dataframe where 'text' is NaN
+    subset_df = df[condition]
+
+    # Define what the 'text' column should be if the condition is met
+    value_when_true = 'Title: ' + subset_df['title'].astype(str) + '. Abstract: ' + subset_df['abstract'].astype(str)
+
+    # Define what the 'text' column should be if the abstract is na
+    value_when_abstract_na = 'Title: ' + subset_df['title'].astype(str) + '.'
+
+    # Apply the conditions to the DataFrame
+    df.loc[condition, 'text'] = np.where(subset_df['abstract'].isna(), value_when_abstract_na, value_when_true)
+
+    print("After calculating text, # text not None: ", len(df[df['text'].notna()]), "# None: ", len(df[df['text'].isna()]))
+
+
+    print("Before calculating embedding, # embedding not None: ", len(df[df['embedding'].notna()]), "# None: ", len(df[df['embedding'].isna()]))
+
+    # Get indices where 'embedding' is None
+    embedding_isna_indices = df[df['embedding'].isna()].index
+    max_index = embedding_isna_indices.max()
+
+    # Compute SPECTER embeddings for these rows and store in 'embedding' column
+    now = datetime.now()
+    date_str = now.strftime('%y-%m-%d')
+    time_str = now.strftime('%H-%M-%S')
+    folder_path = f'papers/{date_str}'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for i, idx in enumerate(embedding_isna_indices):
+        print(f"i {i} idx {idx} / {max_index}")
+        df.loc[idx, 'embedding'] = get_specter_embedding(df.loc[idx, 'text'], tokenizer, model)
+
+        # save to json in papers and date, labeled by time every 500 iterations
+        if (i + 1) % 500 == 0:
+            df.to_json(f'{folder_path}/{time_str}_{df.shape[0]}_get_embeddings_{i}_{MODEL_NAME_SAVE_SAFE}.json', indent=2)
+
+            # save to main
+            df.to_json(papers_json_path)
+
+    print("# embedding not None: ", len(df[df['embedding'].notna()]), "# None: ", len(df[df['embedding'].isna()]))
+
+get_embeddings()
