e972caea6c6a6298b4408def0636346745d5e5ec
Kevin Huang <kevihuang@microsoft.com>
2023-11-02 23:24:57 -0700

    working 1 cycle of multiple functions

--- eureka_exp/llm.py
+++ eureka_exp/llm.py
@@ -6,10 +6,11 @@ import dotenv
 import os
 import openai
 import json
+import time
 
 dotenv.load_dotenv()
 # openai.api_key = os.getenv("OPENAI_GPT4_API_KEY")
-openai.api_key = os.getenv("OPENAI_GPT4_API_KEY")
+openai.api_key = os.getenv("OPENAI_CORP_API_KEY")
 
 # models
 EMBEDDING_MODEL = "text-embedding-ada-003"
@@ -43,73 +44,113 @@ def chat_openai_high_temp(prompt="Tell me to ask you a prompt", model=GPT_MODEL,
     return text_answer, messages
 
 def chat_openai(prompt="Tell me to ask you a prompt", model=GPT_MODEL, chat_history=[], temperature=0, verbose=False, system_prompt="You are a helpful assistant. Answer as correctly, clearly, and concisely as possible.", functions=[], function_call="auto", available_functions={}):
-    # define message conversation for model
-    messages = [
-        {"role": "system", "content": system_prompt},
-    ]
-    if chat_history:
-        messages += chat_history
-    messages.append({"role": "user", "content": prompt})
+    # To deal with rate limits, just wait
+    while True:
+        try:
+            # define message conversation for model
+            messages = [
+                {"role": "system", "content": system_prompt},
+            ]
+            if chat_history:
+                messages += chat_history
+            messages.append({"role": "user", "content": prompt})
 
-    # create the chat completion
-    if verbose:
-        print("Prompt messages: ", messages)
-    
-    if functions:
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=temperature,
-            functions=functions,
-            function_call=function_call,
-        )
-    else:
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=messages,
-            temperature=temperature,
-        )
-    if verbose:
-        print("Completion info: ", completion)
-    response_message = completion["choices"][0]["message"]
+            # create the chat completion
+            if verbose:
+                print("Prompt messages: ", messages)
+            
+            if functions:
+                completion = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    temperature=temperature,
+                    functions=functions,
+                    function_call=function_call,
+                )
+            else:
+                completion = openai.ChatCompletion.create(
+                    model=model,
+                    messages=messages,
+                    temperature=temperature,
+                )
+            if verbose:
+                print("Completion info: ", completion)
+            response_message = completion["choices"][0]["message"]
 
-    # Handle function calls if there are any
-    if "function_call" in response_message and response_message.get("function_call"):
-        if verbose: 
-            print("Entering function call: ", response_message.get("function_call"))
-        # Step 3: call the function
-        # Note: the JSON response may not always be valid; be sure to handle errors
-        function_name = response_message["function_call"]["name"]
-        function_to_call = available_functions[function_name]
-        function_args = json.loads(response_message["function_call"]["arguments"])
-        function_response = function_to_call(**function_args)
+            # Handle function calls if there are any
+            while "function_call" in response_message and response_message.get("function_call"):
+                if verbose: 
+                    print("Entering function call: ", response_message.get("function_call"))
+                # Step 3: call the function
+                # Note: the JSON response may not always be valid; be sure to handle errors
+                function_name = response_message["function_call"]["name"]
+                function_to_call = available_functions[function_name]
+                function_args = json.loads(response_message["function_call"]["arguments"])
+                function_response = function_to_call(**function_args)
 
-        # Step 4: send the info on the function call and function response to GPT
-        # TODO: potentially fix this because there's some funky conversation here.
-        # Commenting out below to potentially address this
-        # messages.append(response_message)  # extend conversation with assistant's reply
+                # Step 4: send the info on the function call and function response to GPT
+                # TODO: potentially fix this because there's some funky conversation here.
+                # Commenting out below to potentially address this
+                # messages.append(response_message)  # extend conversation with assistant's reply
 
-        if verbose:
-            print("Function call output: ", function_response)
+                if verbose:
+                    print("Function call output: ", function_response) # Note: This returns the answer and the full reasoning in skill agent
 
-        messages.append(
-            {
-                "role": "function",
-                "name": function_name,
-                "content": function_response,
-            }
-        )  # extend conversation with function response
+                messages.append(
+                    {
+                        "role": "function",
+                        "name": function_name,
+                        "content": function_response,
+                    }
+                )  # extend conversation with function response
 
-        if verbose:
-            print("Messages now: ", messages)
-        response_message = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-0613",
-            messages=messages,
-        )["choices"][0]["message"]  # get a new response from GPT where it can see the function response
+                if verbose:
+                    print("Messages now: ", messages)
+                response_message = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo-0613",
+                    messages=messages,
+                    temperature=temperature,
+                    functions=functions,
+                    function_call=function_call,
+                )["choices"][0]["message"]  # get a new response from GPT where it can see the function response
 
-        if verbose:
-            print("Function calling response: ", response_message)
+                # Repeat if there's still a function call
 
-        messages.append(response_message) # So you can review the full chat history
+            if verbose:
+                print("Function calling response: ", response_message)
 
-    return response_message, messages
+            messages.append(response_message) # So you can review the full chat history
+
+            return response_message, messages
+        except openai.error.RateLimitError:
+            print(
+                "   *** The OpenAI API rate limit has been exceeded. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.Timeout:
+            print(
+                "   *** OpenAI API timeout occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIError:
+            print(
+                "   *** OpenAI API error occurred. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.APIConnectionError:
+            print(
+                "   *** OpenAI API connection error occurred. Check your network settings, proxy configuration, SSL certificates, or firewall rules. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.InvalidRequestError:
+            print(
+                "   *** OpenAI API invalid request. Check the documentation for the specific API method you are calling and make sure you are sending valid and complete parameters. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        except openai.error.ServiceUnavailableError:
+            print(
+                "   *** OpenAI API service unavailable. Waiting 10 seconds and trying again. ***"
+            )
+            time.sleep(10)  # Wait 10 seconds and try again
+        else:
+            break
